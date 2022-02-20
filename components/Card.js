c485869b7f4f7276594f98ff5cdfa8fddc94ec22
An Quach <quachan888@gmail.com>
2022-02-20 09:30:45 -0500

    Image Placeholder in Card Component

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,8 @@
 import {Text, StyleSheet, View, TouchableOpacity, Image} from 'react-native';
 import React, {Component} from 'react';
 
+const placeholderImage = require('../assets/images/placeholder.png');
+
 export default class Card extends Component {
   render() {
     const {item} = this.props;
@@ -9,10 +11,16 @@ export default class Card extends Component {
         {/* <Text>{item.poster_path}</Text> */}
         <Image
           style={styles.image}
-          source={{
-            uri: `https://image.tmdb.org/t/p/original${item.poster_path}`,
-          }}
+          source={
+            item.poster_path
+              ? {uri: 'https://image.tmdb.org/t/p/original' + item.poster_path}
+              : placeholderImage
+          }
         />
+
+        {!item.poster_path && (
+          <Text style={styles.movieName}>{item.title}</Text>
+        )}
       </TouchableOpacity>
     );
   }
@@ -22,10 +30,18 @@ const styles = StyleSheet.create({
   container: {
     padding: 5,
     position: 'relative',
+    alignItems: 'center',
+    height: 200,
   },
   image: {
     height: 200,
     width: 120,
     borderRadius: 10,
+  },
+  movieName: {
+    position: 'absolute',
+    width: 100,
+    textAlign: 'center',
+    top: 10,
   },
 });
