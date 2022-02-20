37e95fbe895fbadaa9a4f5b405b4859a031a6791
An Quach <quachan888@gmail.com>
2022-02-20 13:27:07 -0500

    Installing React Native Navigation

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native';
+
 import {View, StyleSheet} from 'react-native';
 import React from 'react';
 import Home from './screens/Home';
 
 const App = () => {
     return (
-        <View style={styles.container}>
-            <Home />
-        </View>
+        <NavigationContainer>
+            <View style={styles.container}>
+                <Home />
+            </View>
+        </NavigationContainer>
     );
 };
 
